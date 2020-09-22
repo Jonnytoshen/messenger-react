fcfcf51b5466bebc46fd9ba8b32061703fea6621
Jonny <jonnytoshen@gmail.com>
2020-09-22 16:50:36 +0800

    refactor(NavBar): NavBarLink component instead of a tag

--- src/components/nav-bar/NavBar.tsx
+++ src/components/nav-bar/NavBar.tsx
@@ -1,35 +1,28 @@
 import React from 'react';
 import Icon from '../icon';
+import NavBarLink from '../nav-bar-link';
 import './NavBar.scss';
 
 class NavBar extends React.Component {
   render() {
     return (
       <ul className="navbar">
+        <li className="navbar-item-blank"></li>
         <li className="navbar-item">
-          <a className="navbar-link" href="/" title="Create chat">
-            <Icon type="edit" size="large" />
-          </a>
+          <NavBarLink iconType="edit" iconSize="large" link="/" title="Create chat" />
         </li>
         <li className="navbar-item">
-          <a className="navbar-link" href="/" title="Friends">
-            <Icon type="users" size="large" />
-          </a>
+          <NavBarLink iconType="users" iconSize="large" link="/" title="Friends" />
         </li>
         <li className="navbar-item">
-          <a className="navbar-link" href="/" title="Chats">
-            <Icon type="message-square" size="large" />
-          </a>
+          <NavBarLink iconType="message-square" iconSize="large" link="/" title="Chats" hasDot active />
         </li>
         <li className="navbar-item">
-          <a className="navbar-link" href="/" title="User">
-            <Icon type="user" size="large" />
-          </a>
+          <NavBarLink iconType="user" iconSize="large" link="/" title="User" />
         </li>
+        <li className="navbar-item-blank"></li>
         <li className="navbar-item">
-          <a className="navbar-link" href="/" title="Settings">
-            <Icon type="settings" size="large" />
-          </a>
+          <NavBarLink iconType="settings" iconSize="large" link="/" title="Settings" />
         </li>
       </ul>
     );
