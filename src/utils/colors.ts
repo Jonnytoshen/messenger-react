c4c53c3e1bb28032211fa72491f99b92a5dd85c4
Jonny <jonnytoshen@gmail.com>
2020-10-30 10:45:49 +0800

    feat: add utils

---
+++ src/utils/colors.ts
@@ -0,0 +1,20 @@
+import { tuple } from "./type";
+
+export const PresetStatusColorTypes = tuple('success', 'processing', 'error', 'default', 'warning');
+
+// eslint-disable-next-line import/prefer-default-export
+export const PresetColorTypes = tuple(
+  'pink',
+  'red',
+  'yellow',
+  'orange',
+  'cyan',
+  'green',
+  'blue',
+  'purple',
+  'geekblue',
+  'magenta',
+  'volcano',
+  'gold',
+  'lime',
+);
