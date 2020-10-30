5ab2b439555af5317c4ca039fd31199c8ddf87ee
Jonny <jonnytoshen@gmail.com>
2020-10-30 10:43:25 +0800

    refactor(NavBarLink): use NavLink

--- src/components/nav-bar-link/NavBarLink.tsx
+++ src/components/nav-bar-link/NavBarLink.tsx
@@ -1,32 +1,23 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import Icon, { IconSize } from '../icon';
 import './NavBarLink.scss';
 
 export interface NavBarLinkProps {
   iconType: string,
   iconSize: IconSize,
-  link?: string,
+  link: string,
   title?: string,
-  hasDot?: boolean,
-  active?: boolean
+  hasDot?: boolean
 }
 
 class NavBarLink extends React.Component<NavBarLinkProps> {
-
-  constructor(props: NavBarLinkProps) {
-    super(props);
-  }
-
   render() {
-    const classNames = ['navbar-link'];
-    if (this.props.active) {
-      classNames.push('navbar-link-active');
-    }
     return (
-      <a className={ classNames.join(' ') } href={this.props.link} title={this.props.title}>
+      <NavLink className="navbar-link" activeClassName="navbar-link-active" to={this.props.link} title={this.props.title}>
         <Icon type={this.props.iconType} size={this.props.iconSize} />
         { this.props.hasDot && <span className="navbar-link-dot"></span> }
-      </a>
+      </NavLink>
     );
   }
 }
